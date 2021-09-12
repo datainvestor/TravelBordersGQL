c0d96d18117aa802e42410e52d3bcb670388bd07
alyx <pokepim@zoho.eu>
2021-09-12 17:26:18 +0200

    created countries model

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 #setup flask app
 app = Flask(__name__)
@@ -10,7 +11,12 @@ CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('POSTGRES_DB')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #disable tracking modification of objects and sending signals to the application for every database change
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-#import routes/views - important to import it after setting app and relevant db config
+
+"""
+import routes/views - important to import it after setting app and relevant db config
+place at the bottom of the file (we do not use views here, only ensure its imported)
+"""
 import api.views
 
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from api import app
+from api import app #this is circular import
 from ariadne import load_schema_from_path, make_executable_schema,\
     graphql_sync, snake_case_fallback_resolvers
 from ariadne.constants import PLAYGROUND_HTML
